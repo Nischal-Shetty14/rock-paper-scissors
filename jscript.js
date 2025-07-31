df9c8cfaaf5a8e978dc5acbd6eddda14ee631ab7
Nischal Shetty <y173301163+Nischal-Shetty14@users.noreply.github.com>
2025-08-01 01:43:00 +0530

    complete logic of the game

--- jscript.js
+++ jscript.js
@@ -1,3 +1,5 @@
+let humanscore=0;
+let computerscore=0;
 function getComputerchoice()
 {
 let random=Math.random();
@@ -19,4 +21,44 @@ function getHumanchoice()
     let userinput=prompt("Do you select rock,paper or scissors")
     return userinput;
 }
-console.log(getHumanchoice());
+function playround(humanchoice,computerchoice)
+{
+
+humanchoice=humanchoice.toLowerCase();
+if(humanchoice=="paper"&&computerchoice=="rock"||humanchoice=="rock"&&computerchoice=="scissors"||humanchoice=="scissors"&&computerchoice=="paper")
+{
+    humanscore++;
+    console.log(`You win,${humanchoice} beats ${computerchoice}`);
+}
+else if(computerchoice==humanchoice)
+{
+    console.log("It's a tie");
+}
+else
+{
+    computerscore++;
+    console.log(`Computer wins,${computerchoice} beats ${humanchoice}`);
+}}
+function playgame()
+{
+for(let i=0;i<5;i++)
+{
+    let humanchoice=getHumanchoice();
+    let computerchoice=getComputerchoice();
+    playround(humanchoice,computerchoice);//this is calling itself 5 times with new parameters for a fresh round
+}
+console.log(`Final Score - You: ${humanscore}, Computer: ${computerscore}`);
+if(computerscore>humanscore)
+{
+    console.log("Computer wins");
+}
+else if(humanscore>computerscore)
+{
+    console.log("Human wins");
+}
+else
+{
+    console.log("draw");
+}
+}
+playgame();
